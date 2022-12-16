4c09d02601dd945f1a7c42156bb286d9da7d233b
Daniel Figueroa <68354185+danielFiguePa@users.noreply.github.com>
2022-12-16 11:45:50 -0500

    Calculando descuentos con JavaScript class

---
+++ descuentos.js
@@ -0,0 +1,24 @@
+const inputPrice = document.querySelector('#price');
+const inputDiscount = document.querySelector('#discount');
+const btn = document.querySelector('#calcular');
+const pResult =  document.querySelector('#result');
+
+btn.addEventListener('click', calcularPrecioConDescuento);
+
+function calcularPrecioConDescuento () {
+  const price = Number(inputPrice.value);
+  const discount = Number(inputDiscount.value);
+
+  if(!price || ! discount) {
+    pResult.innerText = 'CHANCLA por favor llena el formulario';
+    return;
+  }
+  if(discount > 100) {
+    pResult.innerText = 'Aja, ya quisieras, no te vamos a dar plata, PAGA!';
+    return;
+  }
+
+  const newPrice = price * (100 - discount) / 100;
+
+  pResult.innerText = 'El nuevo precio con descuento es $' + newPrice;
+}
