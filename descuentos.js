70946a64dc0afd25e08188fc6dd07cdefe32feba
Daniel Figueroa <68354185+danielFiguePa@users.noreply.github.com>
2022-12-16 13:46:39 -0500

    Metodo find vs metodo filter class

--- descuentos.js
+++ descuentos.js
@@ -5,36 +5,57 @@ const pResult =  document.querySelector('#result');
 
 btn.addEventListener('click', calcularPrecioConDescuento);
 
+// OBject solution
+// const couponObj = {
+//   'JuanDC_es_Batman': 30,
+// };
+
+// Array solution
+const couponList = [];
+
+couponList.push({
+  name: 'JuanDC_es_Batman',
+  discount: 30,
+});
+couponList.push({
+  name: 'pero_es_un_secreto',
+  discount: 25,
+});
+couponList.push({
+  name: 'no_le_digas_a_nadie',
+  discount: 15,
+})
+
 function calcularPrecioConDescuento () {
   const price = Number(inputPrice.value);
   const coupon = inputCoupon.value;
 
-  if(!price || ! coupon) {
+  if(!price || !coupon) {
     pResult.innerText = 'CHANCLA por favor llena el formulario';
     return;
   }
 
   let discount;
 
-  // if(coupon == 'JuanDC_es_Batman'){
-  //   discount = 30;
-  // } else if (coupon == 'no_le_digas_a_nadie') {
-  //   discount = 25;
+  // Object solution
+  // if (couponObj[coupon]) {
+  //   discount = couponObj[coupon];
   // } else {
   //   pResult.innerText = 'El cupón no es valido';
   //   return;
   // }
 
-  switch (coupon) {
-    case 'JuanDC_es_Batman':
-      discount = 30;
-      break;
-    case 'no_le_digas_a_nadie':
-      discount = 25;
-      break;
-    default:
-      pResult.innerText = 'El cupón no es valido';
-      return;
+  function isCouponInArray(couponElement) {
+    return couponElement.name == coupon;
+  }
+
+  const couponInArray = couponList.find(isCouponInArray);
+
+  if(couponInArray) {
+    discount = couponInArray.discount;
+  } else {
+    pResult.innerText = 'El cupón no es valido';
+    return;
   }
 
   const newPrice = price * (100 - discount) / 100;
