09800a98ae0a872aa332a97b73ec8ad5472713ae
Daniel Figueroa <68354185+danielFiguePa@users.noreply.github.com>
2022-12-17 14:11:23 -0500

    Calculando el promedio class

---
+++ platzimath.js
@@ -0,0 +1,9 @@
+function calcularPromedio(lista) {
+  let sumaLista = 0;
+  for(let i = 0; i < lista.length; i++) {
+    sumaLista = sumaLista + lista[i];
+  }
+
+  const promedio = sumaLista / lista.length;
+  return promedio;
+}
