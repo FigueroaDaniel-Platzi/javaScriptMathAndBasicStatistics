b8ae57edc315e55c87f8e6bf1590cffa0bd184a3
Daniel Figueroa <68354185+danielFiguePa@users.noreply.github.com>
2022-12-17 14:32:23 -0500

    Método reduce class

--- platzimath.js
+++ platzimath.js
@@ -1,8 +1,19 @@
 function calcularPromedio(lista) {
-  let sumaLista = 0;
-  for(let i = 0; i < lista.length; i++) {
-    sumaLista = sumaLista + lista[i];
+  // Usando ciclos
+  // let sumaLista = 0;
+  // for(let i = 0; i < lista.length; i++) {
+  //   sumaLista = sumaLista + lista[i];
+  // }
+
+  function sumarTodosElementos(valorAcumulado, nuevoValor) {
+    return valorAcumulado + nuevoValor;
   }
+  // Arrow function example
+  // const ejemplo = (a, b) => a + b;
+  // const sumerTodosElementos = (valorAcumulado, nuevoValor) => valorAcumulado + nuevoValor;
+  // const sumaLista = lista.reduce((a, b) => a + b);
+
+  const sumaLista = lista.reduce(sumarTodosElementos); // Usando método reduce  
 
   const promedio = sumaLista / lista.length;
   return promedio;
