4ed403551e94bf03d09fc8f5798d8057a2d18be7
Daniel Figueroa <68354185+danielFiguePa@users.noreply.github.com>
2022-12-17 16:02:57 -0500

    Calculando la mediana en una lista par class

--- platzimath.js
+++ platzimath.js
@@ -1,10 +1,36 @@
+//============== MEDIANA ===================================================================
+function esPar(lista) {
+  return !(lista.length % 2);
+}
+function esImpar(lista) {
+  return lista.length % 2;
+}
+function calcularMediana(lista) {
+  const listaEsPar = esPar(lista);
+
+  if(listaEsPar) {
+    const indexMitad1ListaPar = (lista.length / 2) - 1;
+    const indexMitad2ListaPar = lista.length / 2;
+
+    const listaMitades = [];
+    listaMitades.push(lista[indexMitad1ListaPar]);
+    listaMitades.push(lista[indexMitad2ListaPar]);
+
+    const medianaListaPar = calcularPromedio(listaMitades);
+    return medianaListaPar;
+  } else {
+    const indexMitadListaImpar = Math.floor(lista.length / 2); // Using Math.floor
+    const medianaListaImpar = lista[indexMitadListaImpar];
+    return medianaListaImpar
+  }
+}
+//=============== PROMEDIO ==================================================================
 function calcularPromedio(lista) {
   // Usando ciclos
   // let sumaLista = 0;
   // for(let i = 0; i < lista.length; i++) {
   //   sumaLista = sumaLista + lista[i];
   // }
-
   function sumarTodosElementos(valorAcumulado, nuevoValor) {
     return valorAcumulado + nuevoValor;
   }
@@ -18,3 +44,4 @@ function calcularPromedio(lista) {
   const promedio = sumaLista / lista.length;
   return promedio;
 }
+//===========================================================================================
