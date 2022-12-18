3169866eadd6da904f1b19aec9ca243cebfbc7ce
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-18 15:52:27 -0500

    Calculando la moda: arrays a partir de objetos class

--- platzimath.js
+++ platzimath.js
@@ -61,4 +61,31 @@ function calcularPromedio(lista) {
   const promedio = sumaLista / lista.length;
   return promedio;
 }
-//===========================================================================================
+//================= MODA =====================================================================
+function calcularModa(lista) {
+  const listaCount = {};
+
+  for(let i=0; i < lista.length; i++) {
+    const elemento = lista[i];
+    if(listaCount[elemento]) {
+      listaCount[elemento] += 1;
+    } else {
+      listaCount[elemento] = 1;
+    }
+  }
+
+  const listaArray = Object.entries(listaCount); // Using Object.entries
+  const listaOrdenada = ordenarListaBidimensional(listaArray,1);
+  const listaMaxNumber = listaOrdenada[listaOrdenada.length - 1];
+  const moda = listaMaxNumber[0];
+  
+  return moda;
+}
+
+function ordenarListaBidimensional(listaDesordenada, i) {
+  function ordenarListaSort(valorAcumulado, nuevoValor) {
+    return valorAcumulado[i] - nuevoValor[i];
+  }
+  const lista = listaDesordenada.sort(ordenarListaSort);
+  return lista;
+}
