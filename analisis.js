8a25fb37dbc65d6bd5c63855947066a9a10caed3
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-19 16:46:57 -0500

    Proyección salarial personal class

--- analisis.js
+++ analisis.js
@@ -18,3 +18,24 @@ function medianaPorPersona(nombrePersona) {
   const medianaSalarios = PlatziMath.calcularMediana(salarios);
   return medianaSalarios;
 }
+
+function proyeccionPorPersona(nombrePersona) {
+  trabajos = encontrarPersona(nombrePersona).trabajos;
+
+  let porcentajesCrecimiento = [];
+
+  for(let i = 0; i < trabajos.length; i++) {
+    const salarioActual = trabajos[i].salario;
+    const salarioPasado = trabajos[i - 1];
+    const crecimiento = salarioActual - salarioPasado;
+    const porcentajeCrecimiento = crecimiento / salarioPasado;  
+    porcentajesCrecimiento.push(porcentajeCrecimiento);
+  }
+
+  const medianaPorcentajeCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+  const ultimoSalario = trabajos[trabajos.length - 1];
+  const aumento = ultimoSalario * medianaPorcentajeCrecimiento;
+  const nuevoSalario = ultimoSalario + aumento;
+
+  return nuevoSalario;
+}
